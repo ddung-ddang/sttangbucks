8e2b5db57c23a09b0b2a77a27be8b37af0af5005
EomYerim <djadpfa1002@naver.com>
2024-12-28 01:57:55 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/sttangbucks/SttangbucksApplication.java
@@ -0,0 +1,13 @@
+package com.sttangbucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SttangbucksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SttangbucksApplication.class, args);
+    }
+
+}
